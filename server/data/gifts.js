4dcd60bcc38d4f475081ab90d8358c6c28acf574
Zakariya Ahmed <zakariyaahmed077@gmail.com>
2023-09-29 01:05:11 -0400

    Updated

---
+++ server/data/gifts.js
@@ -0,0 +1,55 @@
+const giftData = [
+    {
+      "id": 1,
+      "name": "Elden Ring",
+      "pricePoint": "$59.99",
+      "audience": "Gamers",
+      "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/header.jpg?t=1683618443",
+      "description": "An adventure-rich and thrilling game",
+      "submittedBy": "Sasha",
+      "submittedOn": "2022-09-04T14:48:00"
+    },
+    {
+      "id": 2,
+      "name": "The Last of Us Part II",
+      "pricePoint": "$59.99",
+      "audience": "Gamers",
+      "image": "https://upload.wikimedia.org/wikipedia/en/4/46/Video_Game_Cover_-_The_Last_of_Us.jpg",
+      "description": "A gripping post-apocalyptic action-adventure game.",
+      "submittedBy": "Player1",
+      "submittedOn": "2023-09-25T14:48:00"
+    },
+    {
+      "id": 3,
+      "name": "God of War (2018)",
+      "pricePoint": "$49.99",
+      "audience": "Gamers",
+      "image": "https://image.api.playstation.com/vulcan/img/rnd/202010/2217/RnDzysOs81fdvhdQAHYtxjFT.png",
+      "description": "Join Kratos on an epic journey in Norse mythology.",
+      "submittedBy": "Player2",
+      "submittedOn": "2023-09-25T12:32:00"
+    },
+    {
+      "id": 4,
+      "name": "Spider-Man: Miles Morales",
+      "pricePoint": "$49.99",
+      "audience": "Gamers",
+      "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1817190/header.jpg?t=1672784176",
+      "description": "Swing through New York City as Miles Morales.",
+      "submittedBy": "Player3",
+      "submittedOn": "2023-09-25T11:01:00"
+    },
+    {
+      "id": 5,
+      "name": "Demon's Souls (2020)",
+      "pricePoint": "$69.99",
+      "audience": "Gamers",
+      "image": "https://upload.wikimedia.org/wikipedia/en/1/11/Demons_Souls_remake_cover_art.jpg",
+      "description": "A remake of the classic action RPG with stunning visuals.",
+      "submittedBy": "Player4",
+      "submittedOn": "2023-09-25T05:23:00"
+    }
+   
+  ];
+  
+  export default giftData;  
---
+++ server/routes/gifts.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import path from 'path'
+import GiftsController from '../controllers/gifts.js'
+
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const router = express.Router()
+
+router.get('/', GiftsController.getGifts)
+
+router.get('/:giftId', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
+})
+
+export default router
